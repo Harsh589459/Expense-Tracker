27a6ac2b87db5c074b53c560465f83626775c9be
harsh589459 <hs589459@gmail.com>
2023-08-07 01:11:36 +0530

    leaderboard optimized

--- controllers/premiumFeatureController.js
+++ controllers/premiumFeatureController.js
@@ -2,33 +2,48 @@ const User = require('../models/userModel');
 const Expense =require('../models/expenseModel')
 
 const Sequelize = require('../util/database');
+const sequelize = require('../util/database');
 
 const getUserLeaderboard = async (req,res)=>{
     try{
-        const users = await User.findAll()
-        const expenses = await Expense.findAll();
-        const userAggregatedExpenses={};
-
-        expenses.forEach((expense)=>{ 
-
-            if(userAggregatedExpenses[expense.userId]){
-                userAggregatedExpenses[expense.userId]+=expense.amount
-
-            }
-            else{
-                userAggregatedExpenses[expense.userId]=expense.amount
-
-            }
+        const leaderBoardofUsers = await User.findAll({
+            attributes:['id','name',[sequelize.fn('sum',sequelize.col('amount')),'total_cost']],
+            include:[
+                {
+                    model:Expense,
+                    attributes:[]
+                }
+            ],
+            group:['id'],//if we take useId then it will only take user id that have expenses and user.id will take all the user
+            order:[['total_cost','DESC']
+            ],
         })
-        var userLeaderBoardDetails=[];
-        users.forEach((user)=>{
-            userLeaderBoardDetails.push({name:user.name,total_cost:userAggregatedExpenses[user.id] || 0 })
-        })
+        // const userAggregatedExpenses = await Expense.findAll({
+        //     attributes:['userId',[sequelize.fn('sum',sequelize.col('amount')),'total_cost']],
+        //     group:['userId']
+        // });
+        // const userAggregatedExpenses={};
+
+        // expenses.forEach((expense)=>{ 
+
+        //     if(userAggregatedExpenses[expense.userId]){
+        //         userAggregatedExpenses[expense.userId]+=expense.amount
+
+        //     }
+        //     else{
+        //         userAggregatedExpenses[expense.userId]=expense.amount
+
+        //     }
+        // })
+        // var userLeaderBoardDetails=[];
+        // users.forEach((user)=>{
+        //     userLeaderBoardDetails.push({name:user.name,total_cost:userAggregatedExpenses[user.id] || 0 })
+        // })
         
-        userLeaderBoardDetails.sort((a,b)=>b.total_cost-a.total_cost)
-        console.log(userLeaderBoardDetails);
+        // userLeaderBoardDetails.sort((a,b)=>b.total_cost-a.total_cost)
+        // console.log(userLeaderBoardDetails);
 
-        res.status(200).json(userLeaderBoardDetails);
+        res.status(200).json(leaderBoardofUsers);
 
     }catch(err){
         console.log(err);
