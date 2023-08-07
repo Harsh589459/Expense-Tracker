0da65c9b26ca1da0093e077abfee84bcf36aa68a
harsh589459 <hs589459@gmail.com>
2023-08-07 13:11:14 +0530

    transaction added in add and delete expense

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -2,6 +2,7 @@ const db =require('../util/database');
 const DailyExpense = require('../models/expenseModel');
 const path = require('path');
 const User = require('../models/userModel')
+const sequelize = require("../util/database");
 
 
 exports.getExpense=async (req,res,next)=>{
@@ -23,8 +24,10 @@ exports.getAllExpense=async (req,res,next)=>{
         return res.status(500).json({error:error,success:false})
     }
 }
-
+//suppost we add expense in database successfully but in User.update it have some error like misspeelled where as were so it will reponse as error and it is not updated in total expense but the expense is added in Daily Expense so it is giving error thats why we use transaction to keep track
 exports.addExpense=async (req,res,next)=>{
+    const t = await sequelize.transaction();
+
     try{
         const{amount,description,category}=req.body;
          await DailyExpense.create({
@@ -32,16 +35,18 @@ exports.addExpense=async (req,res,next)=>{
             description:description,
             category:category,
             userId:req.user.id
-        });
+        },{transaction:t});
         await User.update({
             totalExpenses:Number(req.user.totalExpenses)+ Number(amount),
         },
-        {where:{id:req.user.id}},
+        {where:{id:req.user.id},transaction:t},
         
         )
+        await t.commit();//now the data gets updated
         res.status(200).json({message:"Expenses added"});
 
     }catch(err){
+        await t.roolback();//if error comes then data is not get commited
         console.log(err)
         res.status(500).then("Error occured while adding the expenses")
 
@@ -51,24 +56,28 @@ exports.addExpense=async (req,res,next)=>{
 
 }
 exports.deleteExpense=async (req,res,next)=>{
-    
+    const t = await sequelize.transaction();
+
     const id = req.params.id;
     console.log(id);
     console.log(req.user);
     try{
         const expense = await DailyExpense.findByPk(id);
-        await DailyExpense.destroy({where:{id:id,userId:req.user.id}})
+        await DailyExpense.destroy({where:{id:id,userId:req.user.id},transaction:t})
         await User.update({
             totalExpenses:Number(req.user.totalExpenses)- Number(expense.amount),
         },
-        {where:{id:req.user.id}},
+        {where:{id:req.user.id},transaction:t},
         
         )
+        await t.commit();
         res.status(200).json({message:"Deleted successfully"})
 
     }
     catch(err){
+        await t.roolback();
         console.log(err);
+        res.status(500).then("Error occured while deleting the expenses")
     }
 
 }
